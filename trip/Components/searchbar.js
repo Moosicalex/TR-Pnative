f09ce1f9d541dcf8dfaedbf939649b50cf253a9f
Moosicalex <moosicalex2@gmail.com>
2023-05-08 14:57:59 -0600

    delete useless junk

--- trip/Components/searchbar.js
+++ trip/Components/searchbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Searchbar() {
 
-return();
+return(<></>);
 }
 
 export default Searchbar;
--- trip/pages/home.js
+++ trip/pages/home.js
@@ -4,37 +4,9 @@ import Topbar from 'trip/Components/topbar.js';
 
 function Home() {
 
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '50%'
-    //resizeMode: 'center',
-  },
-  tinyLogo: {
-    width: 50,
-    height: 50,
-  },
-  logo: {
-    width: 66,
-    height: 58,
-  },
-  homescreen_logo: {
-      height: 90,
-      width: 200,
-      resizeMode: 'cover',
-  }
-});
     return (
         <>
         <Topbar/>
-        <View style={styles.container}>
-              <Image
-                style={styles.homescreen_logo}
-                source={require('trip/assets/greenLogo.png')}
-              />
-          </View>
-        <Text>hello sir</Text>
         </>
     );
 }
