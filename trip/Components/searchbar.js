c378204cc94dfc701de1d1d776d409bc509f13a0
Snowager <colepr24@gmail.com>
2023-05-07 19:15:02 -0600

    Update searchbar.js

--- trip/Components/searchbar.js
+++ trip/Components/searchbar.js
@@ -1,26 +1,28 @@
 import React, { useState } from 'react'
-import { TextInput, StyleSheet} from 'react-native'
+import { TextInput, StyleSheet } from 'react-native'
 
 const SearchBar = () => {
     const [content, setContent] = useState("enter text")
 
     return (
         <TextInput
-        editable
-        style={styles.input}
-        onChangeText={setContent}
-        value={content}
+            editable
+            style={styles.input}
+            onChangeText={setContent}
+            value={content}
         />
     )
 }
 
 const styles = StyleSheet.create({
     input: {
-      height: 40,
-      margin: 12,
-      borderWidth: 1,
-      padding: 10,
+        borderRadius: 30,
+        height: 40,
+        width: 300,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
     },
-  });
+});
 
 export default SearchBar
