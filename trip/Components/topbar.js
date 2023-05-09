83fc7c555be64fb59a4c47b6871ddeacdb3819be
Snowager <colepr24@gmail.com>
2023-05-08 18:49:41 -0600

    adds component to avoid keyboard bump

--- trip/Components/topbar.js
+++ trip/Components/topbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {View, Image, StyleSheet, Text, Button} from 'react-native';
-import Searchbar from './searchbar.js';
+import {View, Image, StyleSheet, Text, Button, KeyboardAvoidingView} from 'react-native';
+import Searchbar from './searchBar/searchbar.js';
+import SearchContainer from './searchBar/searchContainer.js';
 
 function Topbar(props) {
 
@@ -9,32 +10,44 @@ navigation = props.navigation;
 const styles = StyleSheet.create({
     container: {
         paddingTop: 15,
+        display: 'flex',
+        flexDirection: 'column',
         backgroundColor: 'green',
         justifyContent: 'center',
         alignItems: 'center',
-        height: '15%',
+        height: '20%',
     },
     bar_logo: {
         height: 50,
         width: 100,
         resizeMode: 'cover',
-    }
+        flexShrink:1
+    },
+    button: {
+        flexShrink:1
+
+    },
 });
     return (
         <>
+        <KeyboardAvoidingView>
         <View style={styles.container}>
             <Button
                 title="Go to Profile page"
                 onPress={() =>
                     navigation.navigate('Profile')
                 }
+                style={styles.button
+                }
             />
             <Image
                 style={styles.bar_logo}
                 source={require('trip/assets/greenLogo.png')}
             />
-            <Searchbar/>
+           
         </View>
+        <SearchContainer/>
+        </KeyboardAvoidingView>
         </>
     );
 }
