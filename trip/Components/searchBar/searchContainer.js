4a4dbca9d0fb9b793b55d99167757fae2136d224
Snowager <colepr24@gmail.com>
2023-05-09 19:25:08 -0600

    remove fakeData

--- trip/Components/searchBar/searchContainer.js
+++ trip/Components/searchBar/searchContainer.js
@@ -7,23 +7,12 @@ import MapSearchBar from "./searchbar";
 
 const SearchContainer = () => {
     const [searchPhrase, setSearchPhrase] = useState("");
-    const fakeData =
-        [
-            { name: "blue" },
-            { name: "red" },
-            { name: "green" },
-            { name: "yellow" }
-        ]
 
     return (
         <SafeAreaView style={styles.root}>
             <MapSearchBar
                 searchPhrase={searchPhrase}
                 setSearchPhrase={setSearchPhrase}
-            />
-            <ListResults
-                searchPhrase={searchPhrase}
-                fakeData={fakeData}
             />
         </SafeAreaView>
     )
@@ -36,10 +25,5 @@ export default SearchContainer
 
 const styles = StyleSheet.create({
     root: {
-        display: 'flex',
-        flexDirection: 'column',
-        flexShrink: 1,
-        justifyContent: "center",
-        alignItems: "center",
     },
 });
