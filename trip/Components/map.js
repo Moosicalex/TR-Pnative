33f709cbd2f615a9f4dd42a1c0ad7e9ff547cc34
Snowager <colepr24@gmail.com>
2023-05-10 19:00:14 -0600

    map loads random center onLoad

--- trip/Components/map.js
+++ trip/Components/map.js
@@ -1,23 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MapView from 'react-native-maps'
 import { StyleSheet, SafeAreaView, View } from 'react-native'
 
 const Map = () => {
 
+    const [lat, setLat] = useState(null)
+    const [lng, setLng] = useState(null)
+    const [onLoad, setOnLoad] = useState(false)
+    
+    const getRnd = (min, max) => {
+        return Math.floor(Math.random() * (max - min) ) + min
+    }
+
 
 
     return (
+        
         <SafeAreaView>
+            {!onLoad ? (setLat(getRnd(-90,90)), setLng(getRnd(-120,120)), setOnLoad(true)) : null}
             <View>
-                <MapView
+                {onLoad && <MapView
                     style={styles.mapStyle}
                     initialRegion={{
-                        latitude: 37.78825,
-                        longitude: -122.4324,
-                        latitudeDelta: 0.0922,
-                        longitudeDelta: 0.0421,
+                        latitude: lat,
+                        longitude: lng,
+                        latitudeDelta: 13,
+                        longitudeDelta: 13,
                     }}
-                />
+                />}
+                
             </View>
         </SafeAreaView>
     )
