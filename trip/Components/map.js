e46727cad836701149199e9469846bb509e0659d
Snowager <colepr24@gmail.com>
2023-05-10 18:09:12 -0600

    add working map

--- trip/Components/map.js
+++ trip/Components/map.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import MapView from 'react-native-maps'
-import { SafeAreaView } from 'react-native/types'
-import { StyleSheet } from 'react-native/types'
+import { StyleSheet, SafeAreaView, View } from 'react-native'
 
 const Map = () => {
 
@@ -18,13 +17,7 @@ const Map = () => {
                         latitudeDelta: 0.0922,
                         longitudeDelta: 0.0421,
                     }}
-
-
                 />
-
-
-
-
             </View>
         </SafeAreaView>
     )
@@ -36,10 +29,7 @@ export default Map
 
 const styles = StyleSheet.create({
     mapStyle: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
+        width: "100%",
+        height: "100%",
     },
 })
