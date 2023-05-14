d4fcb7e5074949f45592ad09f68547d2f0760b83
Moosicalex <moosicalex2@gmail.com>
2023-05-14 17:36:07 -0600

    loading icon added to profile page

--- trip/pages/profile.js
+++ trip/pages/profile.js
@@ -1,21 +1,43 @@
 import React from 'react';
-import {View, Image, StyleSheet, Text} from 'react-native';
+import {View, Image, SafeAreaView, StyleSheet, Text} from 'react-native';
 import Topbar from 'trip/Components/topbar.js';
 import {useQuery} from '@apollo/client';
 import {GET_TRIP_USER_BY_EMAIL} from 'trip/Graphiql/queries.js'
 
 function Profile(props) {
 
-    const {data: user_data, loading: user_loading, error: user_error} = useQuery(GET_TRIP_USER_BY_EMAIL, {
-        variables: {email: props.email},
-        onCompleted: update_found
-      })
+    //const {data: user_data, loading: user_loading, error: user_error} = useQuery(GET_TRIP_USER_BY_EMAIL, {
+     //   variables: {email: props.email},
+     //   onCompleted: update_found
+    //})
 
-      function update_found() {
-        console.log(user_data)
-      }
+    function update_found() {
+    console.log(user_data)
+    }
+    
+
+    const styles = StyleSheet.create({
+        loading_icon: {
+
+
+        },
+        loading_container: {
+            backgroundColor: 'green',
+            height: '100%',
+            flexDirection: 'column',
+            justifyContent: 'center',
+        }
+    })
 
     return (
+        true ? 
+        <>
+            <SafeAreaView style={styles.loading_container} >
+                <Text>Loading</Text>
+                <Image source={require('trip/assets/loading.gif')} />
+            </SafeAreaView>
+        </>
+        :
         <>
         <Topbar navigation={navigation}/>
         <Text>{props.username}</Text>
