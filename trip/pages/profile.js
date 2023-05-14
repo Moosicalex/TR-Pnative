14819aae28a2e28283ac040c2209ae6fb5872301
Moosicalex <moosicalex2@gmail.com>
2023-05-14 17:20:08 -0600

    successful query

--- trip/pages/profile.js
+++ trip/pages/profile.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import {View, Image, StyleSheet, Text} from 'react-native';
 import Topbar from 'trip/Components/topbar.js';
+import {useQuery} from '@apollo/client';
+import {GET_TRIP_USER_BY_EMAIL} from 'trip/Graphiql/queries.js'
 
 function Profile(props) {
+
+    const {data: user_data, loading: user_loading, error: user_error} = useQuery(GET_TRIP_USER_BY_EMAIL, {
+        variables: {email: props.email},
+        onCompleted: update_found
+      })
+
+      function update_found() {
+        console.log(user_data)
+      }
 
     return (
         <>
