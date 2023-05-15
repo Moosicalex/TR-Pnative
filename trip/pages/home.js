5f965d20e9c1e0eb221f77bf1ad0c8c0266cb075
Snowager <colepr24@gmail.com>
2023-05-14 20:48:29 -0600

    add searchbar to home page

--- trip/pages/home.js
+++ trip/pages/home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {View, Image, StyleSheet, Text} from 'react-native';
 import Topbar from 'trip/Components/topbar.js';
+import SearchContainer from '../Components/searchBar/searchContainer';
 
 function Home({navigation}) {
 
     return (
         <>
         <Topbar navigation={navigation}/>
+        <SearchContainer/>
         </>
     );
 }
